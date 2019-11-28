adace1802b208117690c7af9d93c604af8fd9a77
Michael Sterle-Contala <mike.sterle@3box.io>
2019-11-28 17:01:36 -0500

    add fix in case of improper ipfs shutdown

--- ipfs-entrypoint.sh
+++ ipfs-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
+# Run base to init before adding config
 /usr/local/bin/start_ipfs version
+
+# Clean up in case shut down incorrectly
+rm -f $IPFS_PATH/api
 
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://localhost:30000", "https://webui.ipfs.io"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
